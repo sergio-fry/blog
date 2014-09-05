5ab955bf018c71ad66156c0d2315593d716bdff3
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-09-05 20:36:27 +0400

    история как память

--- _plugins/embedly.rb
+++ _plugins/embedly.rb
@@ -8,7 +8,7 @@ module Jekyll
     
     def render(context)
       <<-HTML
-<a class="embedly-card" href="#{@url}">#{@url}</a>
+<a class="embedly-card" data-card-chrome="0" href="#{@url}">#{@url}</a>
 
 <script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>
       HTML
