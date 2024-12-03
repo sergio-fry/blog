87eeda10ae5b15741abd55d449557d55a092f779
Sergei O. Udalov <udalov.x@mail.ru>
2024-12-03 11:34:28 +0300

    Revert "2024-12 ipfs update"
    This reverts commit 06cddc1bac61d20d2eb61ccc39f2bca4472a8020.

--- .build/server/entrypoint.sh
+++ .build/server/entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 if [ "$1" = 'web' ]; then
-  export IPFS_CID=`ipfs --api $KUBO_URL add -qr --only-hash _site | tail -n 1`
   bundle exec rackup -o 0.0.0.0 -p $PORT
 fi
 
--- .build/server/lib/rack/ipfs.rb
+++ .build/server/lib/rack/ipfs.rb
@@ -1,15 +1,26 @@
+require 'ipfs-api'
+
 module Rack 
   class IPFS
-    def initialize(app)
+    def initialize(app, options = {})
       @app = app
+      @kubo_url = options[:kubo_url] || 'http://0.0.0.0:5001'
     end
 
     def call(env)
       _status, headers, _body = response = @app.call(env)
 
-      headers['x-ipfs-path'] = "/ipfs/#{ENV.fetch('IPFS_CID')}#{env['REQUEST_PATH']}"
+      
+      begin
+        headers['x-ipfs-path'] = "/ipfs/#{cid}#{env["REQUEST_PATH"]}"
+      rescue => ex
+        puts ex
+      end
 
       response
     end
+
+    def cid = @cid ||= ipfs.add(Dir.new('_site'))[-1].hash
+    def ipfs = ::IPFS::Connection.new(@kubo_url)
   end
 end
