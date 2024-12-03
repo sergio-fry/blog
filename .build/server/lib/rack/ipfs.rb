06cddc1bac61d20d2eb61ccc39f2bca4472a8020
Sergei O. Udalov <udalov.x@mail.ru>
2024-12-03 11:31:02 +0300

    2024-12 ipfs update

--- .build/server/lib/rack/ipfs.rb
+++ .build/server/lib/rack/ipfs.rb
@@ -1,26 +1,15 @@
-require 'ipfs-api'
-
 module Rack 
   class IPFS
-    def initialize(app, options = {})
+    def initialize(app)
       @app = app
-      @kubo_url = options[:kubo_url] || 'http://0.0.0.0:5001'
     end
 
     def call(env)
       _status, headers, _body = response = @app.call(env)
 
-      
-      begin
-        headers['x-ipfs-path'] = "/ipfs/#{cid}#{env["REQUEST_PATH"]}"
-      rescue => ex
-        puts ex
-      end
+      headers['x-ipfs-path'] = "/ipfs/#{ENV.fetch('IPFS_CID')}#{env['REQUEST_PATH']}"
 
       response
     end
-
-    def cid = @cid ||= ipfs.add(Dir.new('_site'))[-1].hash
-    def ipfs = ::IPFS::Connection.new(@kubo_url)
   end
 end
